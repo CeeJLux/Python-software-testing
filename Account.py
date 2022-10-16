d98964b6f307968c45839413dafeb66c389b533d
Emmanuel Joseph <emmjstud@gmail.com>
2022-10-16 14:01:23 +0100

    update Account oop 1

--- Account.py
+++ Account.py
@@ -1,14 +1,14 @@
 from datetime import date
-print("Bank Related Project")
+print("Bank Related Project\n")
 
-class Account:
+class account:
     def __init__(self, name, account_num, account_bal):
         self.name = name
         self.account_num = account_num
         self.account_bal = account_bal
 
     def show_menu(self):
-        print(f"{self.name}, Welcome to your Dashboard \n Available Balance = {self.account_bal}\n Date: {date.today()}\n")
+        print(f"{self.name}, Welcome to your Dashboard \nAvailable Balance = {self.account_bal}\n Date: {date.today()}\n")
         return "Done"
 
     @property
@@ -18,10 +18,29 @@ class Account:
 
     def deposit(self, amount):
         self.account_bal += amount
-        print(f"Deposit Successful \n Current Balance = {self.account_bal} \n")
+        print(f"Deposit Successful \nCurrent Balance = {self.account_bal} \n")
         return "Done"
 
+    def withdraw(self, amount):
+        if amount > self.account_bal:
+            return "Insufficient Funds"
+        elif amount < self.account_bal:
+            self.account_bal -= amount
+            print(f"Withdrawal Successful \nCurrent balance = {self.account_bal} \n")
+            return "Done"
 
-emmanuel = Account("Emmanuel Joseph", "00000000000", 1000000)
+    def transfer(self, account, amount):
+        if amount > self.account_bal:
+            return "Insufficient Funds"
+        elif amount < self.account_bal:
+            self.account_bal -= amount
+            account.account_bal += amount
+            print(f"Transferred {amount} to {account.name} Successfully\n")
+            return "Done"
 
-print(emmanuel.deposit(3000))
+emmanuel = account("Emmanuel Joseph", "00000000000", 1000000)
+david = account("David Abraham", "00000000001", 1000000)
+print(emmanuel.get_balance)
+print(emmanuel.transfer(david,30000))
+print(emmanuel.get_balance)
+print(david.get_balance)
